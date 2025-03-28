b9661f82bb2dae911deee3493ae3cc8ffb0ba969
GurGergely03 <gergely.gurumlai03@gmail.com>
2025-03-28 18:54:57 +0100

    add cryptocurrency properties

--- CryptoService/Entities/Cryptocurrency.cs
+++ CryptoService/Entities/Cryptocurrency.cs
@@ -2,5 +2,9 @@ namespace CryptoService.Entities;
 
 public class Cryptocurrency
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public float Price { get; set; }
+    public int Available { get; set; }
+    public int Total { get; set; }
 }
