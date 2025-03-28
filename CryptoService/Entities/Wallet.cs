f06c03556e8c7af7c1636d7416ae6bb906e3b74c
GurGergely03 <gergely.gurumlai03@gmail.com>
2025-03-28 18:55:22 +0100

    add wallet properties

--- CryptoService/Entities/Wallet.cs
+++ CryptoService/Entities/Wallet.cs
@@ -2,5 +2,10 @@ namespace CryptoService.Entities;
 
 public class Wallet
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Value { get; set; }
+    public int Amount { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
 }
