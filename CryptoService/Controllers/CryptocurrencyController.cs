83b4b4e77cb4f208953bf6b68089c8106c55773a
GurGergely03 <gergely.gurumlai03@gmail.com>
2025-03-28 19:08:27 +0100

    add cryptocurrency route for all and id based get

--- CryptoService/Controllers/CryptocurrencyController.cs
+++ CryptoService/Controllers/CryptocurrencyController.cs
@@ -1,8 +1,36 @@
+using CryptoService.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CryptoService.Controllers;
 
+
+[Route("api/[controller]")]
+[ApiController]
 public class CryptocurrencyController : Controller
 {
-    
+    private readonly CryptoContext _context;
+
+    public CryptocurrencyController(CryptoContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Cryptocurrency>>> GetCryptocurrencies()
+    {
+        return await _context.Cryptocurrencies.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Cryptocurrency>> GetCryptocurrency([FromRoute] long id)
+    {
+        var cryptocurrency = await _context.Cryptocurrencies.FindAsync(id);
+
+        if (cryptocurrency == null)
+        {
+            return NotFound();
+        }
+        return cryptocurrency;
+    }
 }
