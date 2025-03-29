2e7abe329df610668203d85ce7b762a5143de923
GurGergely03 <gergely.gurumlai03@gmail.com>
2025-03-29 11:49:52 +0100

    add properties to user fields

--- CryptoService/Entities/User.cs
+++ CryptoService/Entities/User.cs
@@ -7,12 +7,21 @@ namespace CryptoService.Entities;
 public class User
 {
     public int Id { get; set; }
+    [MaxLength(30)]
+    [MinLength(3)]
     public string Name { get; set; }
     
     [Required]
     [MaxLength(50)]
+    [MinLength(10)]
     public string Email { get; set; }
+    
+    [MaxLength(20)]
+    [MinLength(5)]
+    [Required]
     public string Password { get; set; }
+    
     public float Balance { get; set; }
+    
     public List<Wallet> Wallets { get; set; } = new List<Wallet>();
 }
