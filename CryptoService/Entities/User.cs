e838adae8db20f4e2fc4d6a624fdcaa41b2ff2b4
GurGergely03 <gergely.gurumlai03@gmail.com>
2025-03-29 11:45:11 +0100

    update user entity
    Made email unique

--- CryptoService/Entities/User.cs
+++ CryptoService/Entities/User.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
 namespace CryptoService.Entities;
 
+[Index(nameof(Email), IsUnique = true)]
 public class User
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    
+    [Required]
+    [MaxLength(50)]
     public string Email { get; set; }
     public string Password { get; set; }
+    public float Balance { get; set; }
     public List<Wallet> Wallets { get; set; } = new List<Wallet>();
 }
