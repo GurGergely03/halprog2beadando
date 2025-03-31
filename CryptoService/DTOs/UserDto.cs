a0390ddc11c8273449b31db4af9688b8aba2adfc
GurGergely03 <gergely.gurumlai03@gmail.com>
2025-03-31 20:10:06 +0200

    Update UserDto.cs
    removed password

--- CryptoService/DTOs/UserDto.cs
+++ CryptoService/DTOs/UserDto.cs
@@ -5,6 +5,5 @@ public class UserDto
     public int Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
-    public string Password { get; set; }
     public float Balance { get; set; }
 }
